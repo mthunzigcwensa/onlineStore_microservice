47e15e27ee1f5b653d07f1ba3c2ac6a0ba37f425
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-05-26 21:26:45 +0200

    Add project files.

---
+++ onlineStore.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using onlineStore.Services.AuthAPI.Models.Dto;
+
+namespace onlineStore.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<bool> AssignRole(string email, string roleName);
+    }
+    
+}
